3931597d1d8e404423df5b74f1bddf9c6b49b108
jocosta96 <jeferson.o.costa@outlook.com>
2025-05-14 07:23:14 -0300

    type updated to value_object

--- app/domain/use_cases/customer_create_uc.py
+++ app/domain/use_cases/customer_create_uc.py
@@ -1,14 +1,14 @@
 from app.domain.use_cases import customer_base_uc
 from app.domain.entities import customer_entity
-from app.domain.value_objects import email_type, document_type, name_type
+from app.domain.value_objects import document, email, name
 
 
 class CustomerCreate(customer_base_uc.BaseCustomerUseCase):
 
     def create(self, customer: customer_entity.Customer) -> int:
 
-        name_type.Name(customer.first_name, customer.last_name).validate()
-        document_type.Document(customer.document).validate()
-        email_type.Email(customer.email).validate()
+        name.Name(customer.first_name, customer.last_name).validate()
+        document.Document(customer.document).validate()
+        email.Email(customer.email).validate()
 
         return self.customer_base_uc.port.create_customer(customer)
